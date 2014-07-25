72f3aa655fed6e631ad291671c9991b9d4ac2a54
MARCUS HSU <mh3346@columbia.edu>
2014-07-25 16:35:29 -0400

    Donecd ..

--- src/marcus/functions/DynamoDBManager_v2.java
+++ src/marcus/functions/DynamoDBManager_v2.java
@@ -12,6 +12,8 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
 import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
@@ -47,7 +49,7 @@ public class DynamoDBManager_v2 {
 
 	public void createTable(String tableName) {
 		if (Tables.doesTableExist(amazonDynamoDBClient, tableName)) {
-			System.out.println("AWS info : Table already exist!!!");
+			// System.out.println("AWS info : Table already exist!!!");
 		} else {
 			CreateTableRequest request = new CreateTableRequest()
 					.withTableName(tableName)
@@ -83,7 +85,8 @@ public class DynamoDBManager_v2 {
 			PutItemRequest putItemRequest = new PutItemRequest(tableName, item);
 			PutItemResult putItemResult = amazonDynamoDBClient
 					.putItem(putItemRequest);
-			System.out.println("AWS info : Put Item Result: " + putItemResult);
+			// System.out.println("AWS info : Put Item Result: " +
+			// putItemResult);
 		}
 	}
 
@@ -128,6 +131,7 @@ public class DynamoDBManager_v2 {
 		ScanRequest request = new ScanRequest().withTableName(tableName);
 		ScanResult result = amazonDynamoDBClient.scan(request);
 		List<Map<String, String>> items = new ArrayList<Map<String, String>>();
+		// return a list of result and also print them on the console
 		for (Map<String, AttributeValue> item : result.getItems()) {
 			items.add(printItem(item));
 		}
@@ -141,17 +145,17 @@ public class DynamoDBManager_v2 {
 		for (Map.Entry<String, AttributeValue> item : attributeList.entrySet()) {
 			String attributeName = item.getKey();
 			AttributeValue value = item.getValue();
-			System.out.println(attributeName
-					+ " "
-					+ (value.getS() == null ? "" : "S=[" + value.getS() + "]")
-					+ (value.getN() == null ? "" : "N=[" + value.getN() + "]")
-					+ (value.getB() == null ? "" : "B=[" + value.getB() + "]")
-					+ (value.getSS() == null ? "" : "SS=[" + value.getSS()
-							+ "]")
-					+ (value.getNS() == null ? "" : "NS=[" + value.getNS()
-							+ "]")
-					+ (value.getBS() == null ? "" : "BS=[" + value.getBS()
-							+ "] \n"));
+			// System.out.println(attributeName
+			// + " "
+			// + (value.getS() == null ? "" : "S=[" + value.getS() + "]")
+			// + (value.getN() == null ? "" : "N=[" + value.getN() + "]")
+			// + (value.getB() == null ? "" : "B=[" + value.getB() + "]")
+			// + (value.getSS() == null ? "" : "SS=[" + value.getSS()
+			// + "]")
+			// + (value.getNS() == null ? "" : "NS=[" + value.getNS()
+			// + "]")
+			// + (value.getBS() == null ? "" : "BS=[" + value.getBS()
+			// + "] \n"));
 			String valueString = (value.getS() == null ? "" : value.getS())
 					+ (value.getN() == null ? "" : value.getN())
 					+ (value.getB() == null ? "" : value.getB())
@@ -164,24 +168,45 @@ public class DynamoDBManager_v2 {
 	}
 
 	// TODO..
-	public void retreiveItem(String tableName) {
+	public Map<String, String> retreiveItem(String tableName, String key,
+			String keyValue) {
 		if (Tables.doesTableExist(amazonDynamoDBClient, tableName)) {
-			System.out.println("Table " + tableName + " exists..");
+			// System.out.println("The following Table name exists: " +
+			// tableName);
 
-			// Scan items
-			ScanRequest scanRequest = new ScanRequest(tableName);
+			Map<String, String> foundItem = null;
+			// Scan items (Approach1: use "scanFilter")
+			HashMap<String, Condition> scanFilter = new HashMap<String, Condition>();
+			String keyValueFormat = "\"" + keyValue + "\"";
+			Condition condition = new Condition().withComparisonOperator(
+					ComparisonOperator.EQ.toString()).withAttributeValueList(
+					new AttributeValue().withS(keyValueFormat));
+			scanFilter.put("stockName", condition);
+			ScanRequest scanRequest = new ScanRequest(tableName)
+					.withScanFilter(scanFilter);
 			ScanResult scanResult = amazonDynamoDBClient.scan(scanRequest);
-			String PK = null;
-			int max = 0;
-			String maxapm = "am";
-			String time;
-			String apm;
-			// scan all the item in the table and then delete them by PK
-			for (Map<String, AttributeValue> item : scanResult.getItems()) {
-
+			// System.out.println(scanResult);
+			if (scanResult.getItems().size() != 0) {
+				foundItem = printItem(scanResult.getItems().get(0));
+				return foundItem;
 			}
+
+			// Scan items (Approach2: use )
+			// ScanRequest scanRequest = new ScanRequest()
+			// .withTableName(tableName);
+			// ScanResult scanResult = amazonDynamoDBClient.scan(scanRequest);
+
+			// System.out.println("Size2: " + scanResult.getItems().size());
+			// for (Map<String, AttributeValue> item : scanResult.getItems()) {
+			// result = printItem(item);
+			// if ((result.get(key).equals("\"" + keyValue+ "\""))) {
+			// System.out.println("the value of the specified key Founded");
+			// }
+			// }
+			System.out.println("Sorry, no such stock name in your Database");
 		} else {
 			System.out.println("The table doesn't exist!!");
 		}
+		return null;
 	}
 }
--- src/marcus/functions/YahooStockApiLauncher.java
+++ src/marcus/functions/YahooStockApiLauncher.java
@@ -4,6 +4,7 @@
 package marcus.functions;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -16,48 +17,84 @@ public class YahooStockApiLauncher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("This tool can help you find any most "
-				+ "updated stock you want and store to AWS "
-				+ "dynamoDB for future use");
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter a stock name : ");
-		// let user input a stock name
-		String stockName = input.nextLine();
-		System.out
-				.println("The following are the current information of the stock : "
-						+ stockName);
-
-		String dbTable = "stocks";
-		int maxDataSize = 20;
-		String[] stockData = new String[maxDataSize];
-		UpdateStockData updateStockData = new UpdateStockData();
-		try {
-			stockData = updateStockData.getAStockData(stockName);
-			// in case the stock doesn't exist
-			if (stockData[2].equals("0.00")) {
-				System.out.println("Sorry, the following stock name you "
-						+ "input doesn't exist in current stock market : "
-						+ stockData[1]);
-				input.close();
-				return;
-			}
-			// System.out.println(stockData.length);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		// updateStockData.storeADataToDynamoDB(dbTable, stockData);
-
+		System.out.println("If you want to search for a current stock's "
+				+ "information, please press 1");
+		System.out.println("If you want to search a stock from your "
+				+ "database, please press 2");
+		int menuChoice = Integer.parseInt(input.nextLine());
 		DynamoDBManager_v2 dbDynamoDBManager = new DynamoDBManager_v2();
-		if (dbTable != null && !dbTable.equals("")) {
-			dbDynamoDBManager.createTable(dbTable);
+		String dbTable = "stocks";
+
+		switch (menuChoice) {
+		case 1:
+			System.out.println("Please enter a stock name : ");
+			// let user input a stock name
+			String stockName = input.nextLine();
+			System.out.println("searching..");
+			System.out.println("The following are the current "
+					+ "information of the stock : " + stockName);
+
+			int maxDataSize = 20;
+			String[] stockData = new String[maxDataSize];
+			UpdateStockData updateStockData = new UpdateStockData();
+			try {
+				stockData = updateStockData.getAStockData(stockName);
+				// in case the stock doesn't exist
+				if (stockData[2].equals("0.00")) {
+					System.out.println("Sorry, the following stock name you "
+							+ "input doesn't exist in current stock market : "
+							+ stockData[1]);
+					input.close();
+					return;
+				}
+				// System.out.println(stockData.length);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			// updateStockData.storeADataToDynamoDB(dbTable, stockData);
+
+			if (dbTable != null && !dbTable.equals("")) {
+				dbDynamoDBManager.createTable(dbTable);
+			}
+
+			System.out.println("Storing and updating your Database..");
+			// Primary Key: stockName
+			String[] keys = new String[] { "openPrice", "stockName",
+					"currentPrice", "tradeDate", "updatedTime" };
+			dbDynamoDBManager.saveAItemToDynamoDB(dbTable, keys, stockData);
+			System.out.println("Update successfully");
+			break;
+
+		case 2:
+			System.out
+					.println("Please enter a stock name to search from your Database : ");
+			String searchStockName = input.nextLine();
+			searchStockName = searchStockName.toUpperCase();
+			Map<String, String> foundStock = null;
+			foundStock = dbDynamoDBManager.retreiveItem(dbTable, "stockName",
+					searchStockName);
+			if (foundStock != null) {
+				System.out.println("stock name: "
+						+ foundStock.get("stockName").substring(1,
+								foundStock.get("stockName").length() - 1));
+				System.out
+						.println("open price: " + foundStock.get("openPrice"));
+				System.out.println("stock name : "
+						+ foundStock.get("stockName"));
+				System.out.println("price now: "
+						+ foundStock.get("currentPrice"));
+				System.out.println("trade date : "
+						+ foundStock.get("tradeDate"));
+				System.out.println("updated time : "
+						+ foundStock.get("updatedTime"));
+				break;
+			}
+		default:
+			break;
 		}
-
-		// Primary Key: stockName
-		String[] keys = new String[] { "openPrice", "stockName",
-				"currentPrice", "tradeDate", "updatedTime" };
-		dbDynamoDBManager.saveAItemToDynamoDB(dbTable, keys, stockData);
-
+		System.out.println("This is the end of the program");
 		input.close();
 
 		// delete all table in AWS DynamoDB
